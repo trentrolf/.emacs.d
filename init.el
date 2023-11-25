e2e0d1e92869cb43fc8a522976a4f4582b4b9262
Trent Rolf <trent.rolf@icloud.com>
2023-11-24 22:31:25 -0700

    delete some stuff

--- init.el
+++ init.el
@@ -13,9 +13,6 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(auto-revert-remote-files t)
  '(confirm-kill-emacs 'yes-or-no-p)
- '(custom-enabled-themes '(dracula))
- '(custom-safe-themes
-   '("1436985fac77baf06193993d88fa7d6b358ad7d600c1e52d12e64a2f07f07176" "c1284dd4c650d6d74cfaf0106b8ae42270cab6c58f78efc5b7c825b6a4580417" "549ccbd11c125a4e671a1e8d3609063a91228e918ffb269e57bd2cd2c0a6f1c6" default))
  '(ediff-diff-options "-w")
  '(ediff-split-window-function 'split-window-horizontally)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
@@ -23,7 +20,7 @@
  '(make-backup-files nil)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   '(vterm meson-mode bbcode-mode bitbake clang-format auto-complete dracula-theme ess julia-repl julia-mode auctex ztree intel-hex-mode markdown-mode ag magit json-mode go-mode))
+   '(vterm meson-mode bbcode-mode bitbake clang-format auto-complete auctex intel-hex-mode markdown-mode ag magit json-mode))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(split-width-threshold 300)
@@ -50,23 +47,7 @@
 (setq-default indent-tabs-mode nil)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(push (substitute-in-file-name "path-to-ztree-directory") load-path)
-(require 'ztree)
-
-(add-hook
- 'eshell-mode-hook
- (lambda ()
-   (setq pcomplete-cycle-completions nil)))
-
-;; (setenv "PATH" (concat (getenv "PATH") ":/home/trolf/hotdrop/external/firmware_common/tools/vutiliti_packet_tool/_build"))
-;; (setenv "PATH" (concat (getenv "PATH") ":/usr/local/go/bin"))
-;; (setenv "PATH" (concat (getenv "PATH") ":/home/trolf/go/bin"))
-
-
 (setq c-default-style "stroustrup")
-
-(put 'magit-clean 'disabled nil)
-(setq magit-diff-refine-hunk (quote all))
 
 ;; Show only one active window when opening multiple files at the same time.
 (add-hook 'window-setup-hook 'delete-other-windows)
@@ -90,8 +71,6 @@
     (find-tag (ido-completing-read "Tag: " tag-names))))
 
 (global-set-key (kbd "C-'") 'ido-find-tag)
-
-(global-auto-complete-mode t)
 
 (setq auto-mode-alist (cons '("\\.bb$" . bitbake-mode) auto-mode-alist))
 
